62e1416875d4c568e6725008e9c579b875a20325
Gerasimets <47577104+Gerasimets@users.noreply.github.com>
2020-02-29 01:04:17 +0300

    Update main.cpp

--- тренировка/main.cpp
+++ тренировка/main.cpp
@@ -1,75 +1,144 @@
+#include <iostream>
+
 // элемент списка
 struct list_element
 {
 	int value;	// значение данного элемента списка
 	list_element* next;	// указатель на следующий элемент списка
+	list_element* prev;	// указатель на предыдущий элемент списка
 };
 
 // список
-struct list
+class list
 {
+public:
+
+	// данные
 	list_element* begin; // указатель на первый элемент списка
 	list_element* end; // указатель на последний элемент списка
 	size_t size;	// количество элементов в списке
+
+	// конструктор
+	list();
+
+	// функции
+	bool empty() const;  // функция проверяет, пуст ли список
+	void push_back(int new_value);  // добавление элемента в конец списка
+	void push_front(int new_value);  // добавление в начало списка
+	void pop_back();  // удаление последнего элемента
+	void pop_front(); // удаление первого элемента
+	int back() const; // возвращает значение последнего элемента
+	int front() const; // возвращает значение первого элемента
 };
 
-// функция проверяет, пуст ли список
-bool empty(const list& list)
+// this - указатель на объект, который вызвал данную фунцию
+
+list::list()
 {
-	if (list.size == 0) return 1;
+	begin = nullptr;
+	end = nullptr;
+	size = 0;
+}
+
+// функция проверяет, пуст ли список
+bool list::empty() const
+{
+	if (size == 0) return 1;
 	return 0;
 }
 
 // добавление элемента в конец списка
-void push_back(list &list, int new_value)
+void list::push_back(int new_value)
 {
 	list_element* new_element = new list_element; // выделить память под переменную типа list_element и присвоить адрес
 													// начала выделенной памяти в переменную-указатель new_element
 	new_element->value = new_value; // пойти по указателю new_element, получить переменную типа list_element, которая там находится
 									// и в ней изменить поле value
 	new_element->next = nullptr;	// добавляем в конец, поэтому следующего элемента нет
+	new_element->prev = end; //указатель на предыдущий элемент указывает на прошлый последний элемент
 	// если список не пустой
-	if (!empty(list))	list.end->next = new_element;	// прежний последний элемент списка теперь указвывает на новый элемент
+	if (!empty())	end->next = new_element;	// прежний последний элемент списка теперь указвывает на новый элемент
 	// если список пустой
-	else list.begin = new_element;  // новый элемент является началом списка			
-	list.end = new_element;	// новый элемент теперь является концом списка
-	list.size++;		// размер списка увеличился на единицу
+	else begin = new_element;  // новый элемент является началом списка			
+	end = new_element;	// новый элемент теперь является концом списка
+	size++;		// размер списка увеличился на единицу
 }
 
 // добавление в начало списка
-void push_front(list& list, int new_value)
+void list::push_front(int new_value)
 {
 	list_element* new_element = new list_element;   // выделить память под переменную типа list_element и присвоить адрес
 													// начала выделенной памяти в переменную-указатель new_element
 	new_element->value = new_value; // пойти по указателю new_element, получить переменную типа list_element, которая там находится
 									// и в ней изменить поле value
-	new_element->next = list.begin; // теперь указателю на следующий элемент списка присваивается прошлый первый элемент списка 
-	list.begin = new_element; // новый элемент является началом списка
+	new_element->next = begin; // теперь указателю на следующий элемент списка присваивается прошлый первый элемент списка 
+	new_element->prev = nullptr; //указатель на предыдущий элемент пустой, так как добавляем первый элемент
+	begin = new_element; // новый элемент является началом списка
 	//если список пустой
-	if (empty(list)) list.end = new_element; // новый элемент теперь является концом списка
-	list.size++; // размер списка увеличился на единицу
+	if (empty()) end = new_element; // новый элемент теперь является концом списка
+	size++; // размер списка увеличился на единицу
 }
- 
+
 // удаление последнего элемента, возвращает значение этого элемента
-int pop_back(list& list)
+void list::pop_back()
 { 
-	list_element* cur_element = list.begin; // переменная-указатель(текущий элемент) типа list_element указывает на начало списка
-	while (cur_element->next->next != nullptr) //пока это не предпоследний элемент
+	list_element* cur_element = begin; // переменная-указатель(текущий элемент) типа list_element указывает на начало списка
+	if (size != 1)
 	{
-		cur_element = cur_element->next; // двигаемся по элементам списка, пока не найдем предпоследний 
+		while (cur_element->next != end) //пока это не предпоследний элемент
+			cur_element = cur_element->next; // двигаемся по элементам списка, пока не найдем предпоследний 
+		delete cur_element->next;
+		cur_element->next = nullptr;
+		end = cur_element;
 	}
-
+	else
+	{
+		delete cur_element;
+		begin = nullptr;
+		end = nullptr;
+	}
+	size--;
 }
 
-// удаление первого элемента, возвращает значение этого элемента
-int pop_front(list& list)
+void list::pop_front()
 {
+	list_element* element_to_delete = begin;
+	begin = begin->next;
+	if (size==1) end = nullptr;
+	delete element_to_delete;
+	size--;
+}
 
+int list::back() const
+{
+	return end->value;
+}
+
+int list::front() const
+{
+	return begin->value;
 }
 
 int main()
 {
-	list MyList{ nullptr,nullptr,0 };
-	push_back(MyList,7);
-	push_back(MyList, 11);
+	list MyList;
+	MyList.push_back(7);
+	MyList.push_back(11);
+	MyList.push_back(17);
+	MyList.push_back(25);
+	MyList.push_front(3);
+	MyList.push_front(1);
+	if (!MyList.empty())	std::cout << MyList.front() << '\n';
+	MyList.pop_front();
+	if (!MyList.empty())	std::cout << MyList.front() << '\n';
+	MyList.pop_front();
+	if (!MyList.empty())	std::cout << MyList.front() << '\n';
+	MyList.pop_front();
+	if (!MyList.empty())	std::cout << MyList.front() << '\n';
+	MyList.pop_front();
+	if (!MyList.empty())	std::cout << MyList.front() << '\n';
+	MyList.pop_front();
+	if (!MyList.empty())	std::cout << MyList.front() << '\n';
+	MyList.pop_front();
+	if (!MyList.empty())	std::cout << MyList.front() << '\n';
 }
